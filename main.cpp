5e08e6526b4c7631936a282040b057921cebd507
Marcelo Barreto <marcelohrnq@gmail.com>
2024-07-21 13:03:16 -0300

    create print method

--- main.cpp
+++ main.cpp
@@ -24,17 +24,42 @@ private:
         Node(int val) : value(val), left(nullptr), right(nullptr) {}
     };
 
-    Node* root;
+    Node* root; // Root Node
 
+    // Private method to insert a value in the tree
     Node* insert(Node* node, int value) {
+
+        // Check if Node is null
         if (!node) return new Node(value);
 
+        // Check if Value is to insert in the left or right
         if (value < node->value)
             node->left = insert(node->left, value);
         else
             node->right = insert(node->right, value);
 
         return node;
+    }
+
+    // Private method to show the tree 
+    void print(Node* node, int space) {
+        // Check if node is null
+        if (!node) return;
+
+        // Increases space for formatting
+        space += 10;
+
+        // Print Right Node
+        print(node->right, space);  
+        cout << endl;
+
+        for (int i = 10; i < space; i++) cout << " ";
+
+        // Print Node value
+        cout << node->value << "\n";
+
+        // Print Left Node
+        print(node->left, space);
     }
 };
 
